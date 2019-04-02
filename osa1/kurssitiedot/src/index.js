3269f21729090ef2ef3a94c9921bc174849aac3d
Yki <suomenkansa@gmail.com>
2019-04-02 14:22:05 +0300

    osa1 kurssitiedot

---
+++ osa1/kurssitiedot/src/index.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = ({ name }) => {
+  return (
+    <>
+      <h1>{name}</h1>
+    </>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
+    </>
+  )
+}
+
+const Part = ({ part, exercises }) => {
+  return (
+    <>
+      <p>
+        {part} {exercises}
+      </p>
+    </>
+  )
+}
+
+const Total = ({ parts }) => {
+  const count = parts.reduce((a, b) => { return a + b.exercises }, 0)
+  return (
+    <>
+      <p>yhteensä {count} tehtävää</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack -sovelluskehitys',
+    parts: [
+      {
+        name: 'Reactin perusteet',
+        exercises: 10
+      },
+      {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7
+      },
+      {
+        name: 'Komponenttien tila',
+        exercises: 14
+      }
+    ]
+  }
+
+
+
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
